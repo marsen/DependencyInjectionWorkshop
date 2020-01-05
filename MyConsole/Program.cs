6e9b12a87930bef5c3843871aa7f6d58247a5c2e
marklin <marklin@gmail.com>
2020-01-05 12:44:45 +0800

    remove uselesscode

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -1,50 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Autofac;
-using Autofac.Core;
 using DependencyInjectionWorkshop.Models;
 using DependencyInjectionWorkshop.Models.Decorator;
 using DependencyInjectionWorkshop.Models.Interface;
 
 namespace MyConsole
 {
-    class Program
+    internal class Program
     {
-
-        private static IAuthentication _authentication;
-        private static IFailedCounter _failedCounter;
-        private static IHash _hash;
-        private static ILogger _logger;
-        private static INotification _notification;
-        private static IOtpService _otpService;
-        private static IProfile _profile;
         private static IContainer _container;
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-
-            //_otpService = new FakeOtp();
-            //_hash = new FakeHash();
-            //_profile = new FakeProfile();
-            //_logger = new FakeLogger();
-            //_notification = new FakeSlack();
-            //_failedCounter = new FakeFailedCounter();
-            //_authentication =
-            //    new AuthenticationService(_profile, _hash,_otpService);
-
-            //_authentication = new FailedCounterDecorator(_authentication, _failedCounter);
-            //_authentication = new LoggerDecorator(_authentication, _logger, _failedCounter);
-            //_authentication = new NotificationDecorator(_authentication, _notification);
-
             RegisterContainer();
-            _authentication = _container.Resolve<IAuthentication>();
-            var isValid = _authentication.Verify("joey", "abc", "wrong otp");
+            var authentication = _container.Resolve<IAuthentication>();
+            var isValid = authentication.Verify("joey", "abc", "wrong otp");
             Console.WriteLine($"result:{isValid}");
             Console.ReadLine();
-
         }
 
         private static void RegisterContainer()
@@ -62,9 +34,8 @@ namespace MyConsole
             builder.RegisterDecorator<LoggerDecorator, IAuthentication>();
             builder.RegisterDecorator<NotificationDecorator, IAuthentication>();
 
-             _container = builder.Build();
+            _container = builder.Build();
         }
-
 
         internal class FakeLogger : ILogger
         {
@@ -96,10 +67,9 @@ namespace MyConsole
 
             public void Add(string accountId)
 
-            { 
+            {
                 Console.WriteLine($"{nameof(FakeFailedCounter)}.{nameof(Add)}({accountId})");
             }
-
 
             public int Get(string accountId)
             {
@@ -121,7 +91,6 @@ namespace MyConsole
                 Console.WriteLine($"{nameof(FakeOtp)}.{nameof(GetOtp)}({accountId})");
                 return "123456";
             }
-
         }
 
         internal class FakeHash : IHash
@@ -131,7 +100,6 @@ namespace MyConsole
                 Console.WriteLine($"{nameof(FakeHash)}.{nameof(ComputeHash)}({plainText})");
                 return "my hashed password";
             }
-
         }
 
         internal class FakeProfile : IProfile
@@ -141,7 +109,6 @@ namespace MyConsole
                 Console.WriteLine($"{nameof(FakeProfile)}.{nameof(Password)}({accountId})");
                 return "my hashed password";
             }
-
         }
     }
 }
