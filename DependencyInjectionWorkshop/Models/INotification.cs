4b22777155fa75c8faa5f0909421ea8296dc363e
marklin <marklin@gmail.com>
2020-01-05 10:43:39 +0800

    create file

---
+++ DependencyInjectionWorkshop/Models/INotification.cs
@@ -0,0 +1,12 @@
+﻿namespace DependencyInjectionWorkshop.Models
+{
+    public interface INotification
+    {
+        /// <summary>
+        /// Notifies the specified account identifier.
+        /// </summary>
+        /// <param name="accountId">The account identifier.</param>
+        /// <param name="message"></param>
+        void Notify(string accountId, string message);
+    }
+}
--- DependencyInjectionWorkshop/Models/SlackAdapter.cs
+++ DependencyInjectionWorkshop/Models/SlackAdapter.cs
@@ -2,16 +2,6 @@
 
 namespace DependencyInjectionWorkshop.Models
 {
-    public interface INotification
-    {
-        /// <summary>
-        /// Notifies the specified account identifier.
-        /// </summary>
-        /// <param name="accountId">The account identifier.</param>
-        /// <param name="message"></param>
-        void Notify(string accountId, string message);
-    }
-
     public class SlackAdapter : INotification
     {
         public SlackAdapter()
