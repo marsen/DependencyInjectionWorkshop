d492e889772c00b0b386ea3d213159c373b94093
marklin <marklin@gmail.com>
2020-01-07 01:25:02 +0800

    create new files with class

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,76 +1,8 @@
 ﻿using System;
 using System.Net.Http;
-using SlackAPI;
 
 namespace DependencyInjectionWorkshop.Models
 {
-    public class OtpService
-    {
-        public string CurrentOtp(string accountId, HttpClient httpClient)
-        {
-            //get otp
-            var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception($"web api error, accountId:{accountId}");
-            }
-
-            var currentOtp = response.Content.ReadAsAsync<string>().Result;
-            return currentOtp;
-        }
-    }
-
-    public class FailedCounter
-    {
-        public void ResetFailedCount(string accountId, HttpClient httpClient)
-        {
-            var resetResponse = httpClient.PostAsJsonAsync("api/failedCounter/Reset", accountId).Result;
-            resetResponse.EnsureSuccessStatusCode();
-        }
-
-        public void CheckIsLocked(string accountId, HttpClient httpClient)
-        {
-            //check account locked
-            var isLockedResponse = httpClient.PostAsJsonAsync("api/failedCounter/IsLocked", accountId).Result;
-
-            isLockedResponse.EnsureSuccessStatusCode();
-            if (isLockedResponse.Content.ReadAsAsync<bool>().Result)
-            {
-                throw new FailedTooManyTimesException() { AccountId = accountId };
-            }
-        }
-
-        public void AddFailedCount(string accountId, HttpClient httpClient)
-        {
-            //失敗
-            var addFailedCountResponse = httpClient.PostAsJsonAsync("api/failedCounter/Add", accountId).Result;
-            addFailedCountResponse.EnsureSuccessStatusCode();
-        }
-
-        public int GetFailedCount(string accountId, HttpClient httpClient)
-        {
-            //紀錄失敗次數
-            var failedCountResponse =
-                httpClient.PostAsJsonAsync("api/failedCounter/GetFailedCount", accountId).Result;
-
-            failedCountResponse.EnsureSuccessStatusCode();
-
-            var failedCount = failedCountResponse.Content.ReadAsAsync<int>().Result;
-            return failedCount;
-        }
-    }
-
-    public class SlackNotifyService
-    {
-        public void Notify(string accountId)
-        {
-            //notify
-            string message = $"account:{accountId} try to login failed";
-            var slackClient = new SlackClient("my api token");
-            slackClient.PostMessage(messageResponse => { }, "my channel", message, "my bot name");
-        }
-    }
-
     public class NLogger
     {
         public void Log(string message)
@@ -115,7 +47,7 @@ namespace DependencyInjectionWorkshop.Models
                 var failedCount = _failedCounter.GetFailedCount(accountId, httpClient);
                 _nLogger.Log($"accountId:{accountId} failed times:{failedCount}");
 
-                _slackNotifyService.Notify(accountId);
+                _slackNotifyService.Notify(accountId, $"account:{accountId} try to login failed");
 
                 return false;
             }
---
+++ DependencyInjectionWorkshop/Models/SlackNotifyService.cs
@@ -0,0 +1,14 @@
+﻿using SlackAPI;
+
+namespace DependencyInjectionWorkshop.Models
+{
+    public class SlackNotifyService
+    {
+        public void Notify(string message)
+        {
+            //notify
+            var slackClient = new SlackClient("my api token");
+            slackClient.PostMessage(messageResponse => { }, "my channel", message, "my bot name");
+        }
+    }
+}
