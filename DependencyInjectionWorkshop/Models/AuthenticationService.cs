3977bfed82919d867b237db14c29a3c045980c5a
marklin <marklin@gmail.com>
2020-01-07 02:35:04 +0800

    rename field

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -7,19 +7,13 @@ namespace DependencyInjectionWorkshop.Models
     public class AuthenticationService
     {
         private readonly IProfileInfo _profileInfo = new ProfileInfo();
-        private readonly IHash _sha256Hash = new SHA256Hash();
+        private readonly IHash _hash = new SHA256Hash();
         private readonly IOtpService _otpService = new OtpService();
         private readonly IFailedCounter _failedCounter = new FailedCounter();
-        private readonly INotify _slackNotifyService = new SlackNotifyService();
-        private readonly ILogger _nLogger = new NLogger();
+        private readonly INotify _notifyService = new SlackNotifyService();
+        private readonly ILogger _logger = new NLogger();
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AuthenticationService" /> class.
-        /// </summary>
-        public AuthenticationService()
-        {
-            _profileInfo = new ProfileInfo();
-        }
+        
         public bool Verify(string accountId, string password, string otp)
         {
             var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
@@ -28,7 +22,7 @@ namespace DependencyInjectionWorkshop.Models
 
             var passwordFromDb = _profileInfo.Password(accountId);
 
-            var hashedPassword = _sha256Hash.HashedPassword(password);
+            var hashedPassword = _hash.HashedPassword(password);
 
             var currentOtp = _otpService.CurrentOtp(accountId, httpClient);
 
@@ -44,9 +38,9 @@ namespace DependencyInjectionWorkshop.Models
                 _failedCounter.Add(accountId, httpClient);
 
                 var failedCount = _failedCounter.Get(accountId, httpClient);
-                _nLogger.Log($"accountId:{accountId} failed times:{failedCount}");
+                _logger.Log($"accountId:{accountId} failed times:{failedCount}");
 
-                _slackNotifyService.Notify( $"account:{accountId} try to login failed");
+                _notifyService.Notify( $"account:{accountId} try to login failed");
 
                 return false;
             }
